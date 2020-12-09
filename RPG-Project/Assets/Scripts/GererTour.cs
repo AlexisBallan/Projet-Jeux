ab8868c17ae72e54772ab652874b4657da40607f
Alexis <allexis.bal@hotmail.fr>
2020-12-09 02:19:48 -0500

    Initial commit

---
+++ RPG-Project/Assets/Scripts/GererTour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GererTour : MonoBehaviour
+{
+    public Personnage Perso1;
+    public Personnage Perso2;
+
+    private Personnage JoueurActif;
+    
+}
